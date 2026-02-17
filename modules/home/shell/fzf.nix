33fb59fb3465cb9b7cd9c85c66364d9cce69d615
Sharlon N. Regales <sharlonregales@gmail.com>
2026-02-17 20:12:27 +0100

    add a fuzzy finder

---
+++ modules/home/shell/fzf.nix
@@ -0,0 +1,46 @@
+_: {
+  flake.modules.homeManager.fzf = _: {
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      defaultOptions = [
+        "--margin=1"
+        "--layout=reverse"
+        "--border=none"
+        "--info=hidden"
+        "--prompt='/ '"
+        "--no-bold"
+        "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+        "--preview-window=right:60%:wrap"
+      ];
+    };
+
+    programs.zsh.shellAliases = {
+      fvim = "fzf --bind='enter:become(nvim {})'";
+    };
+
+    programs.zsh.initContent = ''
+      function _fzf_edit() {
+        local file
+        file="$(fzf --walker-root="''${1:-.}" --bind='enter:become(echo {})')"
+        if [ -n "$file" ]; then
+          ''${EDITOR:-nvim} "$file"
+        fi
+        zle reset-prompt
+      }
+
+      function _fzf_cwd_widget() { _fzf_edit; }
+      function _fzf_home_widget() { _fzf_edit ~; }
+
+      function _fzf_zvm_keybindings() {
+        zvm_define_widget _fzf_cwd_widget
+        zvm_define_widget _fzf_home_widget
+        # <leader>f: fzf -> $EDITOR in cwd (normal mode)
+        zvm_bindkey vicmd ' f' _fzf_cwd_widget
+        # <leader>F: fzf -> $EDITOR in home (normal mode)
+        zvm_bindkey vicmd ' F' _fzf_home_widget
+      }
+      zvm_after_lazy_keybindings_commands+=(_fzf_zvm_keybindings)
+    '';
+  };
+}
