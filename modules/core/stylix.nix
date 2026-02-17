fc767444b90caba1469799f3e3d5a15f4470cde0
Sharlon N. Regales <sharlonregales@gmail.com>
2026-02-17 20:18:00 +0100

    add fira code nerd font
    realize stylix was not set on vm

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -9,6 +9,10 @@
         base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
         image = config.lib.stylix.pixel "base0A";
         polarity = "dark";
+        fonts.monospace = {
+          package = pkgs.nerd-fonts.fira-code;
+          name = "FiraCode Nerd Font Mono";
+        };
       };
     };
 }
