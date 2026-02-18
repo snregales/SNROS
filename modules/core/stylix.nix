0dd7309ca01d94c97ab0e40ce1e1c4a324d8d91f
Sharlon N. Regales <sharlonregales@gmail.com>
2026-02-18 15:55:56 +0100

    Add system-wide fonts configuration

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -1,18 +1,33 @@
-{ inputs, ... }:
-{
-  flake.modules.nixos.stylix =
-    { pkgs, config, ... }:
-    {
-      imports = [ inputs.stylix.nixosModules.stylix ];
-      stylix = {
-        enable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
-        image = config.lib.stylix.pixel "base0A";
-        polarity = "dark";
-        fonts.monospace = {
+{inputs, ...}: {
+  flake.modules.nixos.stylix = {
+    pkgs,
+    config,
+    ...
+  }: {
+    imports = [inputs.stylix.nixosModules.stylix];
+    stylix = {
+      enable = true;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+      image = config.lib.stylix.pixel "base0A";
+      polarity = "dark";
+      opacity.terminal = .7;
+      fonts = {
+        monospace = {
           package = pkgs.nerd-fonts.fira-code;
           name = "FiraCode Nerd Font Mono";
         };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+        sizes = {
+          applications = 10;
+          terminal = 10;
+          desktop = 10;
+          popups = 10;
+        };
       };
+      autoEnable = true;
     };
+  };
 }
