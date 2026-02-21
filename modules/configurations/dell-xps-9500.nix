047e2902d10f77f1defdf87c189a8d1c1fcf8a54
Sharlon N. Regales <sharlonregales@gmail.com>
2026-02-21 10:05:43 +0100

    make dell xps configurations more DRY

--- modules/configurations/dell-xps-9500.nix
+++ modules/configurations/dell-xps-9500.nix
@@ -6,81 +6,14 @@
   inherit (config) flake;
 in {
   configurations.nixos.dell-xps-9500 = {
-    module = {lib, ...}: {
-      imports = with flake.modules.nixos; [
-        inputs.nixos-hardware.nixosModules.dell-xps-15-9500
-        _1password
-        base
-        biometrics
-        cachix
-        packages
-        disko
-        keyboard
-        greetd
-        impermanence
-        nh
-        niri
-        nix-index-database
-        noctalia
-        snregales
-        sops
-        stylix
-        zfs
+    module = _: {
+      imports = [
+        inputs.nixos-hardware.nixosModules.dell-xps-15-9500-nvidia
+        flake.modules.nixos.dell-xps
       ];
-
-      snros.user = {
-        name = "Sharlon N. Regales";
-        email = "sharlonregales@gmail.com";
-        sshPublicKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtZXJQLbehQuw2Dsmjy2Ko3yimTZr/GljTooplRgH9v snregales@git"
-        ];
-      };
 
       networking.hostName = "dell-xps-9500";
       networking.hostId = "99a0aaa7";
-
-      # Boot loader
-      boot.loader = {
-        limine.enable = true;
-        efi.canTouchEfiVariables = true;
-      };
-
-      # VM variant overrides
-      virtualisation.vmVariant = {
-        services.fprintd.enable = false;
-        disko.enableConfig = false;
-        boot = {
-          initrd.postDeviceCommands = lib.mkForce "";
-          loader = {
-            limine.enable = lib.mkForce false;
-            efi.canTouchEfiVariables = lib.mkForce false;
-          };
-        };
-        environment.persistence = lib.mkForce {};
-        fileSystems."/persist" = lib.mkForce {
-          device = "tmpfs";
-          fsType = "tmpfs";
-        };
-
-        virtualisation = {
-          memorySize = 8192;
-          cores = 4;
-          diskSize = 32768;
-
-          qemu.options = [
-            "-device virtio-vga-gl"
-            "-display gtk,gl=on"
-          ];
-
-          sharedDirectories.sops-key = {
-            source = ''"''${SOPS_AGE_KEY_DIR}"'';
-            target = "/var/lib/sops-nix";
-            securityModel = "none";
-          };
-        };
-      };
-
-      system.stateVersion = "25.05";
     };
   };
 }
---
+++ modules/configurations/dell-xps.nix
@@ -0,0 +1,76 @@
+{config, ...}: let
+  inherit (config) flake;
+in {
+  flake.modules.nixos.dell-xps = {lib, ...}: {
+    imports = with flake.modules.nixos; [
+      _1password
+      base
+      biometrics
+      cachix
+      packages
+      disko
+      keyboard
+      greetd
+      impermanence
+      nh
+      niri
+      nix-index-database
+      noctalia
+      snregales
+      sops
+      stylix
+      zfs
+    ];
+
+    snros.user = {
+      name = "Sharlon N. Regales";
+      email = "sharlonregales@gmail.com";
+      sshPublicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtZXJQLbehQuw2Dsmjy2Ko3yimTZr/GljTooplRgH9v snregales@git"
+      ];
+    };
+
+    # Boot loader
+    boot.loader = {
+      limine.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    # VM variant overrides
+    virtualisation.vmVariant = {
+      services.fprintd.enable = lib.mkForce false;
+      disko.enableConfig = false;
+      boot = {
+        initrd.postDeviceCommands = lib.mkForce "";
+        loader = {
+          limine.enable = lib.mkForce false;
+          efi.canTouchEfiVariables = lib.mkForce false;
+        };
+      };
+      environment.persistence = lib.mkForce {};
+      fileSystems."/persist" = lib.mkForce {
+        device = "tmpfs";
+        fsType = "tmpfs";
+      };
+
+      virtualisation = {
+        memorySize = 8192;
+        cores = 4;
+        diskSize = 32768;
+
+        qemu.options = [
+          "-device virtio-vga-gl"
+          "-display gtk,gl=on"
+        ];
+
+        sharedDirectories.sops-key = {
+          source = ''"''${SOPS_AGE_KEY_DIR}"'';
+          target = "/var/lib/sops-nix";
+          securityModel = "none";
+        };
+      };
+    };
+
+    system.stateVersion = "26.05";
+  };
+}
